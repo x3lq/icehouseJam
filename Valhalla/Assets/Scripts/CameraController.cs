00090ac4df878fc3041c0b176ae96353cfa4698d
Robin Ostner <Ostner_Robin@web.de>
2019-11-11 13:37:02 +0100

    Camera Controller

---
+++ Valhalla/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	[Header("References")]
+	public CharacterMovement character;
+
+	[Header("Settings")]
+	public float distance;
+	public float speed;
+
+	[Header("Status")]
+	public Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		transform.position = character.transform.position + Vector3.back * distance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Follow();
+    }
+
+	void Follow()
+	{
+		targetPosition = character.transform.position + Vector3.back * distance;
+		transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
+	}
+}
