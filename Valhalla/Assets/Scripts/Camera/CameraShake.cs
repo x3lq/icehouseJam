a41ec60636bba7c31afeeca14feaf0174b7883ab
x3lq <webmaster@x3lq.de>
2019-11-12 23:21:23 +0100

    Adding ScreenShake scene, and screenshake functionality

---
+++ Valhalla/Assets/Scripts/Camera/CameraShake.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using Random = UnityEngine.Random;
+
+public class CameraShake : MonoBehaviour
+{
+    //shakes Camera based on duration and magnitude
+    //script has to applied to a game object, which has the camera as a child
+
+    /*private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shakeFixedNumber(3, 3);
+        }
+    }*/
+
+    public void shake(float duration, float magnitude)
+    {
+        StartCoroutine(shakeHelper(duration, magnitude / 100f));
+    }
+
+    public void shakeFixedNumber(float amount, float magnitude)
+    {
+        while (amount > 0)
+        {
+            amount -= 1;
+            StartCoroutine(shakeHelper(0.2f, magnitude / 100f));
+
+        }
+    }
+
+    IEnumerator shakeHelper(float duration, float magnitude)
+    {
+        Vector3 originalPosition = transform.localPosition;
+
+        float elapsed = 0;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+            
+            transform.localPosition = originalPosition + new Vector3(x, y, 0);
+            
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = originalPosition;
+    }
+}
