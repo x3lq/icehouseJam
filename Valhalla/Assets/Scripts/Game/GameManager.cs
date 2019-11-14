2371b2312be4313a26ba4000cdbdd43538b9a696
x3lq <webmaster@x3lq.de>
2019-11-15 00:32:16 +0100

    adding game Manager and necessary scenes

---
+++ Valhalla/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown("Start"))
+        {
+            //ToDo show Menu
+        }
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void playerLost()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void playerWon()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
